85e0eccb2ec7236640f768769036fc6aca982519
Gabrielle L. Damondamon <damondamon.gl@obmontessori.edu.ph>
2025-09-04 10:39:14 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from pyscript import display
+
+restaurant_name = "Potato-Leeks Restaurant" #This is the name of the restaurant
+owner_name = "Damondamon, Gabrielle, Liwag" #This is the owner of the restaurant
+year_established = 2025 #This is the year the restaurant was established
+popular_item_price = 12.99 #This is the price of the most popular item on the menu
+product_names = {"Potato-Leek Soup", "Creamy and Cheesy Mashed Potatoes", "Creamy Leek Pasta"} #This is a list of popular items on the menu
+business_hours = "10:20 AM - 9:00 PM" #This is the business hours of the restaurant
+menu_prices = [5.99, 7.99, 12.99, 15.99] #This is a list of prices for items on the menu
+has_delivery = False #This indicates if the restaurant has delivery service
+common_allergens = {"Gluten", "Dairy", "Nuts"} #This is a list of common allergens in the menu items
+tax_rates = ["0.05", "0.1", "0.15"] #This is a list of tax rates applicable to the restaurant
+
+display(owner_name)
+display(year_established)
+display(business_hours)
+display(common_allergens)
+display(menu_prices)
+# Displaying all the variables
